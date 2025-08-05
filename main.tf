f2908be1ae4f791f53fbbcf4cf860d9cba0288a1
kishormraghavani <kishor.raghavani@hotmail.com>
2025-08-05 15:11:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "https://github.com/kishormraghavani/fluffy-barnacle.git"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatekishor"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
@@ -25,3 +25,4 @@ resource "azurerm_resource_group" "example" {
   name     = "RG2"
   location = "westeurope"
 }
+
